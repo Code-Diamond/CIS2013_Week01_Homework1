2d8b97bcf5500d476b71602203d0bc557fec4529
Testing <testing@testing.test>
2018-01-24 22:30:33 -0600

    My business card.

---
+++ rabbitHole.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+	//A loop that iterates 24 times
+	for (int iterator = 0; iterator <= 24;  iterator++)
+	{
+		// print the dash 24 times
+		cout << "-";
+	}
+
+	//print a new line
+	cout << "\n";
+	
+	//print my business card
+	cout << "|      Jamin Ghata      |" << "\n" << "| github.com/JaminROCK  |" << "\n";
+
+	//print out 24 more dashes
+	for (int iterator = 0; iterator <= 24; iterator++)
+	{
+		cout << "-";
+	}
+
+
+	return 0;
+
+
+}
